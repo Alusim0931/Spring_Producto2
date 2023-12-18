04a10b4cd20b07b93962f90dc161392df035f7c6
rauls <alusim0931@ieselcaminas.org>
2023-12-18 19:47:55 +0100

    Menu web funciona, se puede añadir, modificar y elimnar (feo)

---
+++ src/main/java/com/example/spring_product2/controllers/MenuController.java
@@ -0,0 +1,52 @@
+package com.example.spring_product2.controllers;
+
+import com.example.spring_product2.model.entities.Menu;
+import com.example.spring_product2.model.services.IMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/menus")
+public class MenuController {
+
+    @Autowired
+    private IMenuService menuService;
+
+    @GetMapping("/list")
+    public String listMenus(Model model) {
+        List<Menu> menus = menuService.findAll();
+        model.addAttribute("menus", menus);
+        return "list";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Menu menu = new Menu();
+        model.addAttribute("menu", menu);
+        return "form";
+    }
+
+    @PostMapping("/save")
+    public String saveMenu(@ModelAttribute("menu") Menu menu) {
+        menuService.save(menu);
+        return "redirect:/menus/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("menuId") Long id, Model model) {
+        Menu menu = menuService.findById(id);
+        model.addAttribute("menu", menu);
+        return "form";
+    }
+
+    @GetMapping("/delete")
+    public String deleteMenu(@RequestParam("menuId") Long id) {
+        Menu menu = menuService.findById(id);
+        menuService.delete(menu);
+        return "redirect:/menus/list";
+    }
+}
