baab33455d9af2681bde0f7c57ed46130ca5097b
rauls <alusim0931@ieselcaminas.org>
2024-01-08 20:35:38 +0100

    FinalEntities 2.0

--- src/main/java/com/example/spring_product2/model/entity/Menu_has_product.java
+++ src/main/java/com/example/spring_product2/model/entity/Menu_has_product.java
@@ -1,5 +1,24 @@
 package com.example.spring_product2.model.entity;
+import jakarta.persistence.*;
 
-public class Menu_has_product {
+@Entity
+    @Table(name = "Menu_has_product")
+    public class Menu_has_product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "prodcutid")
+    private Long productid;
+
+    @Column(name = "menuid")
+    private Long menuid;
+
+    public void setProductid(Long productid) {this.productid = productid;}
+    public Long getProductid() {return productid;}
+
+    public void setMenuid(Long menuid) {this.productid = menuid;}
+    public Long getMenuid() {return menuid;}
+
+
 
 }
